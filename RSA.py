d776f461911cbdc56a7ba1bbbe79d4442b352f2b
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-02-01 17:00:16 -0500

    Added capability to work with messages that are inputted as strings

--- RSA.py
+++ RSA.py
@@ -1,4 +1,5 @@
 import subprocess
+import binascii
 
 
 def egcd(a, b):
@@ -12,7 +13,7 @@ def egcd(a, b):
 def modInverse(a, m):
     g, x, y = egcd(a, m)
     if g != 1:
-        raise Exception('modular inverse does not exist')
+        raise Exception("Modular inverse does not exist for {} {}".format(a, m))
     else:
         return x % m
 
@@ -35,13 +36,24 @@ n = p * q
 phi_n = (p - 1) * (q - 1)
 d = modInverse(e, phi_n)
 
-m = 110101101101101101110110
+message = input()
+message_bytes = message.encode('utf-8')
+h = binascii.hexlify(message_bytes)
+encoded_message = int(h, 16)
+print("message = {}\n".format(encoded_message))
 
-C = encrypt(m, e, n)
+cipher_text = encrypt(encoded_message, e, n)
+print("ciphertext = {}\n".format(cipher_text))
 
-print("Ciphertext: ", C)
+decrypted = decrypt(cipher_text, d, n)
+h2 = hex(decrypted)
+h3 = h2[2:]
+b2 = h3.encode('ascii')
+b3 = binascii.unhexlify(b2)
+plaintext = b3.decode('utf-8')
 
-print("Plaintext: ", decrypt(C, d, n))
+print("plaintext = {}\n".format(plaintext))
+
 
 
 
