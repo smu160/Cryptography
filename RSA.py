2d08ce980c31c2de6636b59d38f6da07ca708d18
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-12-16 15:14:05 -0500

    Cleaned up code

--- RSA.py
+++ RSA.py
@@ -1,14 +1,12 @@
 import subprocess
 import binascii
 
-
 def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
     else:
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
-
 
 def modInverse(a, m):
     g, x, y = egcd(a, m)
@@ -17,47 +15,45 @@ def modInverse(a, m):
     else:
         return x % m
 
-
 def encrypt(m, e, n):
     return pow(m, e, n)
-
 
 def decrypt(C, d, n):
     return pow(C, d, n)
 
+def encode(m):
+    message_bytes = message.encode("utf-8")
+    hexlifyed_message = binascii.hexlify(message_bytes)
+    encoded_message = int(hexlifyed_message, 16)
+    return encoded_message
 
-p = subprocess.run(["openssl", "prime", "-generate", "-bits", "2048"], stdout=subprocess.PIPE)
-q = subprocess.run(["openssl", "prime", "-generate", "-bits", "2048"], stdout=subprocess.PIPE)
+def decode(encoded_message):
+    h2 = hex(encoded_message)
+    h3 = h2[2:]
+    b2 = h3.encode("ascii")
+    b3 = binascii.unhexlify(b2)
+    message = b3.decode("utf-8")
+    return message
 
-e = 65537
-p = int(p.stdout)
-q = int(q.stdout)
-n = p * q
-phi_n = (p - 1) * (q - 1)
-d = modInverse(e, phi_n)
+if __name__ == "__main__":
 
-message = input()
-message_bytes = message.encode('utf-8')
-h = binascii.hexlify(message_bytes)
-encoded_message = int(h, 16)
-print("message = {}\n".format(encoded_message))
+    p = subprocess.run(["openssl", "prime", "-generate", "-bits", "2048"], stdout=subprocess.PIPE)
+    q = subprocess.run(["openssl", "prime", "-generate", "-bits", "2048"], stdout=subprocess.PIPE)
 
-cipher_text = encrypt(encoded_message, e, n)
-print("ciphertext = {}\n".format(cipher_text))
+    e = 65537
+    p = int(p.stdout)
+    q = int(q.stdout)
+    n = p * q
+    phi_n = (p - 1) * (q - 1)
+    d = modInverse(e, phi_n)
 
-decrypted = decrypt(cipher_text, d, n)
-h2 = hex(decrypted)
-h3 = h2[2:]
-b2 = h3.encode('ascii')
-b3 = binascii.unhexlify(b2)
-plaintext = b3.decode('utf-8')
+    message = input()
+    encoded_message = encode(message)
+    print("encoded message = {}\n".format(encoded_message))
 
-print("plaintext = {}\n".format(plaintext))
+    cipher_text = encrypt(encoded_message, e, n)
+    print("ciphertext = {}\n".format(cipher_text))
 
-
-
-
-
-
-
-
+    decrypted_message = decrypt(cipher_text, d, n)
+    plaintext_message = decode(decrypted_message)
+    print("plaintext message = {}".format(plaintext_message))
